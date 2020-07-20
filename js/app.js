8019a895c8bfb3644574005f59399bd23ff8c196
Christophe Delobel <cdelobel.ext@simplon.co>
2020-07-20 11:11:40 +0200

    init

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const mess = "Hello from app.js";
+console.log(mess);
+
+const totalCredit = document.querySelector("#totalCredit").innerText.split(" ");
+console.log(Number(totalCredit[0]));
+//....
